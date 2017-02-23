07b7f1dc788e090d3951cd786fc3b7aed6cb62f5
Duy Lam <ddl315@nyu.edu>
2017-02-22 20:30:56 -0500

    add detection stages for enemies, detection angles, add lateral movement for player

--- Assets/Scripts/EnemyBehaviorPositive.cs
+++ Assets/Scripts/EnemyBehaviorPositive.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class EnemyBehaviorPositive : MonoBehaviour {
 
-	public Color rayColor;
 	Ray detectRay;
 	public float detectRange;
 
@@ -17,16 +16,51 @@ public class EnemyBehaviorPositive : MonoBehaviour {
 	bool pauseRotate = false;
 	float timePaused;
 
+	int detectTimer = 0;
+	public Transform player;
+
+	[SerializeField] int detectionState = 0;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void FixedUpdate(){
+		//Ray casting in front of enemy, detecting the player if within range
+		//1. calculate vector3 between player and enemy
+		Vector3 directionToPlayer = player.position - this.transform.position;
+
+		//cone detection thing
+		//2. get the angle between the enemy's forward direction and the direction to player
+		//3. if that angle is less than 15, cast a ray
+		//4. if that ray hits something, check the tag of the hit-thing
+		//5. if the hit-thing is the player, debug-log and turn enemy to yellow, and start timer
+		if (Vector3.Angle (this.transform.forward, directionToPlayer) <= 15f) {
+			//Debug.Log (Vector3.Angle (this.transform.forward, directionToPlayer));
+			RaycastHit hit;
+			detectRay = new Ray (this.transform.position, directionToPlayer);
+			Debug.DrawRay (detectRay.origin, detectRay.direction * 10f, Color.red);
+			if (Physics.Raycast (detectRay, out hit, detectRange) == true) {
+				if (hit.collider.tag == "Player") {
+					Debug.Log ("You are within range!");
+					if (detectionState == 0) {
+						this.GetComponent<MeshRenderer> ().material.color = Color.yellow;
+						detectionState = 1;
+						StartCoroutine (TimerCoroutine ());
+					}
+				} else if (detectionState == 1){
+					detectionState = 3;
+				}
+			} else if (detectionState == 1){
+				detectionState = 3;
+			}
+		}
+
+	}
 	// Update is called once per frame
 	void Update () {
 		//enemy rotating at place
-
-
 		enemyRotation = GetComponent<Transform>().localEulerAngles;
 
 		//first if statement: if the enemy reaches the rotate limit, then set the pauseRotate bool to be true and flip the rotate direction. Then, set the 
@@ -45,31 +79,39 @@ public class EnemyBehaviorPositive : MonoBehaviour {
 			pauseRotate = false;
 		}
 
-		Debug.Log (timePaused);
-		Debug.Log (pauseRotate);
+		//Debug.Log (timePaused);
+		//Debug.Log (pauseRotate);
 
 		if (!pauseRotate) {
 			transform.Rotate (0, rotateSpeed * Time.deltaTime, 0);
 		}
 			//Debug.Log (enemyRotation.y);
-
-		//enemyRotation += rotateSpeed * Time.deltaTime;
-
-
-		//Vector3 forward = transform.TransformDirection (Vector3.) * 10f;
-
-		//Ray casting in front of enemy, detecting the player if within range
-
-		RaycastHit hit;
-		detectRay = new Ray (transform.position, transform.forward);
-		Debug.DrawRay (transform.localPosition, transform.forward * detectRange, rayColor);
-
-		if (Physics.Raycast (detectRay, out hit, detectRange)) {
-			if (hit.collider.tag == "Player") {
-				Debug.Log ("You are detected!");
-			}
+		if (detectionState == 2) {
+			Debug.Log ("They've detected you!");
+			this.GetComponent<MeshRenderer> ().material.color = Color.red;
+			//detectionState = 3; //UPDATE A GAMEMANAGER BOOLEAN to tell that YOU'VE LOST
 		}
-		Debug.DrawRay (transform.localPosition, transform.forward * 100f, rayColor);
+
+	}
+
+	IEnumerator TimerCoroutine(){
+		while (true) {
+			detectTimer++;
+
+			yield return new WaitForSeconds (1f);
+
+			if (detectTimer >= 2 && detectionState == 1) {
+				Debug.Log ("They see you!");
+				detectionState = 2;
+				break;
+			}
+			if (detectTimer >= 3 && detectionState == 3) {
+				Debug.Log ("They stop looking at you.");
+				this.GetComponent<MeshRenderer> ().material.color = Color.white;
+				detectionState = 0;
+			}
+			//Debug.Log ("Timer ended!");
+		}
 	}
 }
 
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -16,8 +16,8 @@ public class PlayerBehavior : MonoBehaviour {
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
 
-		charControl.Move (transform.forward * Time.deltaTime * vertical * 4f);
-		charControl.Move (transform.forward * Time.deltaTime * horizontal * 4f);
+		charControl.Move ((transform.forward * vertical + transform.right * horizontal) * 10f * Time.deltaTime);
+		//charControl.Move (transform.forward * Time.deltaTime * vertical * 4f);
 		//charControl.transform.Rotate (0f, horizontal * Time.deltaTime * 90f, 0f);
 		charControl.transform.Rotate (0f, Input.GetAxis ("Mouse X") * Time.deltaTime * 180f, 0f);
 	}
